342ba4e16dd7f677a4a3010ed7d2f01b31f36937
ApolloZhu <public-apollonian@outlook.com>
2017-03-26 03:23:51 -0400

    Merge conflict

--- HackYourHealth/WelcomeViewController.swift
+++ HackYourHealth/WelcomeViewController.swift
@@ -11,69 +11,45 @@ import HealthKit
 
 class WelcomeViewController: UIViewController {
     let healthKitStore: HKHealthStore = HKHealthStore()
-    
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     //MARK:Functions
     @IBAction func requestHealthPermissions(_ sender: Any)
     {
-       authorizeHealthKit()
+        authorizeHealthKit()
     }
-    
+
     func authorizeHealthKit() {
-        
+
         // State the health data type(s) we want to read from HealthKit.
         let healthDataToRead = Set(arrayLiteral:HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!)
         // State the health data type(s) we want to write from HealthKit.
         let healthDataToWrite = Set(arrayLiteral: HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!)
-        
-<<<<<<< HEAD
-        let healthStore = HKHealthStore ()
-        //let sharableTypes = Set([])
-        //let readableTypes = Set([])
-        //healthStore.requestAuthorization(toShare: sharableTypes, read: readableTypes) completion success,error in {
-        
-       // }
-    }
-=======
+
         // Just in case OneHourWalker makes its way to an iPad...
         if !HKHealthStore.isHealthDataAvailable()
         {
             print("Can't access HealthKit.")
         }
-        
-        
+
         // Request authorization to read and/or write the specific data.
-       healthKitStore.requestAuthorization(toShare: healthDataToWrite, read: healthDataToRead)
-        { //[weak self]
-                (success, error) in
-                
-                if !success {
-                    
-                    print("You didn't allow HealthKit to access these read/write data types. In your app, try to handle this error gracefully when a user decides not to provide access. The error was: \(error). If you're using a simulator, try it on a device.")
-                }
-            
-           // self?.performSegue(withIdentifier: "showBasicInfoView", sender: nil)
-
+        healthKitStore.requestAuthorization(toShare: healthDataToWrite, read: healthDataToRead)
+        { (success, error) in
+            if !success {
+                print("You didn't allow HealthKit to access these read/write data types. In your app, try to handle this error gracefully when a user decides not to provide access. The error was: \(error). If you're using a simulator, try it on a device.")
+            }
         }
-        
         performSegue(withIdentifier: "showBasicInfoView", sender: nil)
-        
-        
-
-}
-
->>>>>>> master
-
-    
+    }
 }
