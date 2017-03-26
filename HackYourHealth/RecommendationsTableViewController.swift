e0f7b86fe789576638bf099c4ebc8260b6a248ec
michelewang <michele.wang99@gmail.com>
2017-03-26 10:10:34 -0400

    Shows which improvements you can make but not their details

--- HackYourHealth/RecommendationsTableViewController.swift
+++ HackYourHealth/RecommendationsTableViewController.swift
@@ -9,13 +9,12 @@
 import UIKit
 
 class RecommendationsTableViewController: UITableViewController {
-<<<<<<< HEAD
-
+    
     static let lowRestingHeartRate = HealthRecommendation (title:"Extremely Low Resting Heart Rate", healthyValue: 50, summary:"Your resting heart rate is less than 50 BPM. This is unnaturally low and it is recommended that you seek a docotor", higherValueIsUnhealthy: false)
     
     static let highRestingHeartRate = HealthRecommendation(title: "Higher than usual resting heart rate", healthyValue: 72, summary: "You have a resting heart rate that is higher than usual. This is usually caused by lack of aerobic exercises. It is recommended that you particiate in acitives that exercise your aerobic system such as Walking, Jogging, or Biking", higherValueIsUnhealthy: true)
     
-    static let lowActivityFactor = HealthRecommendation(title: "Low Activity Factor", healthyValue: 1.55, summary: "Your health can be increased by paricipating in excersie more often", higherValueIsUnhealthy: false)
+    static let lowActivityFactor = HealthRecommendation(title: "Low Activity Factor", healthyValue: 1.55, summary: "Your health can be increased by paricipating in exercise more often", higherValueIsUnhealthy: false)
     
     static let lowCaloricIntake =  HealthRecommendation(title: "Low Caloric Intake", healthyValue: DataManager.shared.TDEE!, summary: "You don't seem to be intaking enough calories. We recommend eating more food", higherValueIsUnhealthy: false)
     
@@ -26,7 +25,7 @@ class RecommendationsTableViewController: UITableViewController {
     static let highBMI =  HealthRecommendation(title: "High BMI", healthyValue: 24.9, summary: "You have a higher body mass index than is recommended for your height. Weight loss is suggested to optimize your health. ", higherValueIsUnhealthy: true)
 
     
-   /* var current: Double = 0
+    /*var current: Double = 0
 =======
      var recommendations = [HealthRecommendation] ()
     
@@ -49,29 +48,32 @@ class RecommendationsTableViewController: UITableViewController {
             recommendations.append(highRestingHeartRate)
         }
         
-        let lowActivityFactor = HealthRecommendation(title: "Low Activity Factor", healthyValue: 1.55, summary: "Your health can be incrased by paricipating in excersie more often", higherValueIsUnhealthy: false)
+        let lowActivityFactor = HealthRecommendation(title: "Low Activity Factor", healthyValue: 1.55, summary: "Your health can be incrased by paricipating in exercise more often", higherValueIsUnhealthy: false)
         
         if lowActivityFactor.shouldRecommendHealthImprovement(currentHealthValue: Double(DataManager.shared.activity!.rawValue))
         {
             recommendations.append(lowActivityFactor)
         }
-<<<<<<< HEAD
     }*/
-    
     private var potentialRecommendations: [HealthRecommendation] = [lowBMI,highBMI,highCaloricIntake,lowCaloricIntake,lowActivityFactor,highRestingHeartRate,lowRestingHeartRate]
-
-    private var recommendations: [HealthRecommendation] = []
     
-    for (HealthRecommendation recommendation:potentialRecommendations)
-    {
+    private var recommendations: [HealthRecommendation] = []
+    override func viewDidLoad() {
+
+
+    
+    //for (HealthRecommendation recommendation :potentialRecommendations)
+    //{
             //find out the right recommendation value for the recommendation EX:Heart rate for lowRestingHeartRate
     
-            if (recommendations.shouldRecommendHealthImprovement())
-            {
+            //if (recommendation.shouldRecommendHealthImprovement())
+            //{
+                //display to TableViewController
+    
             //Test recommendation values
-            }
-=======
-        
+    
+            //}
+    
         let lowCaloricIntake =  HealthRecommendation(title: "Low Caloric Intake", healthyValue: DataManager.shared.TDEE!, summary: "You don't seem to be intaking enough calories. We recommend eating more food", higherValueIsUnhealthy: false)
         
         if lowCaloricIntake.shouldRecommendHealthImprovement(currentHealthValue: DataManager.shared.calorieIntake!)
@@ -100,15 +102,13 @@ class RecommendationsTableViewController: UITableViewController {
             recommendations.append(highBMI)
         }
         
-        
-        
-
->>>>>>> origin/Recommendations
+   // }
     }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return recommendations.count
     }
-
+    
 
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
     // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
@@ -118,11 +118,8 @@ class RecommendationsTableViewController: UITableViewController {
     {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
 
-<<<<<<< HEAD
         cell.textLabel?.text = "\(recommendations[indexPath.row])"
-=======
         cell.textLabel?.text = "\(recommendations[indexPath.row].title)"
->>>>>>> origin/Recommendations
 
         return cell
     }
